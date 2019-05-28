a689e59db0a29cf4dc17977dcf72dbbe39067f97
francosorbello <franco.sorbelo@gmail.com>
2019-05-28 11:58:41 -0300

    Añadido el constructor a la clase Units

--- PanchosBattle/Assets/Scripts/Units.cs
+++ PanchosBattle/Assets/Scripts/Units.cs
@@ -15,7 +15,34 @@ public class Units:MonoBehaviour {
 	bool SeRealizoUnaAccion;
 	/*############################## Constructor ##############################*/
 	public Units( TipoUnidad tipoUnidad , int playerNumero ) {
-
+		perteneJugador=playerNumero;
+		switch (tipoUnidad)
+		{
+			case TipoUnidad.Guerrero:
+				vida=80;
+				damage=20;
+				multiplicadorDeDaño=3;
+				movimiento=3;
+				rangoAtaque=1;
+				break;
+			case TipoUnidad.Arquero:
+				vida=60;
+				damage=14;
+				multiplicadorDeDaño=3;
+				movimiento=3;
+				rangoAtaque=3;
+				break;
+			case TipoUnidad.Jinete:
+				vida=100;
+				damage=20;
+				multiplicadorDeDaño=3;
+				movimiento=5;
+				rangoAtaque=1;
+				break;			
+			default:
+				Debug.Log("Error! No existe este tipo de unidad.");
+				break;
+		}
 	}
 	/*############################## Getters && Setters ##############################*/
 	public int Vida { get => vida; set => vida = value; }
@@ -28,9 +55,10 @@ public class Units:MonoBehaviour {
 	public int PerteneJugador { get => perteneJugador; set => perteneJugador =  value ; }
 	/*############################## Metodos ##############################*/
 	public void MostrarRangoDeMovimiento() {
-
+		int rango = this.movimiento;
+		
 	}
-	public void MostrarRanboDeAtaque(){
+	public void MostrarRangoDeAtaque(){
 
 	}
 }
