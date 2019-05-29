c6fc9f8c0d713870b1c542b827de7e895e321d82
Tomás Rinaldi <tomirinaldi8@gmail.com>
2019-05-29 15:27:34 -0300

    Tile ScriptableObject contains all the tile info instead of the generator

---
+++ PanchosBattle/Assets/Scripts/Board/TilemapTile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "New Board Tile", menuName = "Board Tile")]
+public class TilemapTile : TileBase
+{
+    [SerializeField]
+    private Sprite sprite = default;
+
+    [SerializeField]
+    private Color color = Color.white;
+
+    [SerializeField]
+    private Vector3 scale = Vector3.one;
+
+    [SerializeField]
+    private Tile.Info tileInfo = default;
+    public Tile.Info TileInfo => tileInfo;
+
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        tileData.sprite = sprite;
+        tileData.flags = TileFlags.LockAll;
+        tileData.color = color;
+        tileData.transform = Matrix4x4.Scale(scale);
+    }
+}
