e334f88cff67243692104940dbaccb2011257d80
Sebastian Geroli <sebastiangeroli@outlook.com>
2019-05-29 20:38:43 -0300

    Revert "Merge pull request #3 from SebastianGeroli/InicioProyecto"
    This reverts commit 414f13fcf7c7d04515abbadea41ba573a83d0362, reversing
    changes made to ecc75c309ad393e4a8666296f1792635f1bc837b.

--- PanchosBattle/Assets/Scripts/BattleRoyale.cs
+++ PanchosBattle/Assets/Scripts/BattleRoyale.cs
@@ -9,27 +9,27 @@ public class BattleRoyale{
 	public int ContadorDeVeces { get => contadorDeVeces; set => contadorDeVeces = value; }
 	/*############################## Metodos ##############################*/
 	/*Este metodo recorre el array y pinta las casillas avisando cuales van a ser destruidas luego*/
-	public void MostrarIndicador( GameObject[,] tiles) {
+	public void MostrarIndicador( Tile[,] tiles) {
 		for( int x = contadorDeVeces; x < tiles.GetLength( 0 ); x++ ) {
 			for( int y = contadorDeVeces; y < tiles.GetLength( 1 ); y++ ) {
-				tiles[x , y].GetComponent<Tile2>().SpriteRender2.color = Color.red;
+				tiles[x , y].SpriteRenderer.color = Color.red;
 			}
 		}
 		contadorDeVeces += 1;
 		for( int x = contadorDeVeces; x < tiles.GetLength( 0 )-contadorDeVeces; x++ ) {
 			for( int y = contadorDeVeces; y < tiles.GetLength( 1 )-contadorDeVeces; y++ ) {
-				tiles[x , y].GetComponent<Tile2>().SpriteRender2.color = Color.white;
+				tiles[x , y].SpriteRenderer.color = Color.white;
 			}
 
 		}
 	}
 	/*Este metodo recorre el array y las tiles marcadas por color son destruidas*/
-	public void DestruirTiles( GameObject[,]tiles) {
+	public void DestruirTiles( Tile[,]tiles) {
 		for(int x = 0;x<tiles.GetLength(0);x++){
 			for( int y = 0; y < tiles.GetLength( 1 ); y++ ) {
-				if( tiles[x , y].GetComponent<Tile2>().SpriteRender2.color == Color.red ) {
+				if( tiles[x , y].SpriteRenderer.color == Color.red ) {
 					//tiles[x , y].SpriteRenderer.enabled = false;
-					//tiles[x , y].enabled = false;
+					tiles[x , y].enabled = false;
 				}
 			}
 		}
