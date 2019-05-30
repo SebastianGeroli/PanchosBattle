422920f5d3a82ade7bcb396f915ecd152e81a079
Tomás Rinaldi <tomirinaldi8@gmail.com>
2019-05-29 21:57:01 -0300

    Arreglado un bug en el que no se generaban las tiles por un script perdido

--- PanchosBattle/Assets/Scripts/Board.cs
+++ PanchosBattle/Assets/Scripts/Board.cs
@@ -3,95 +3,95 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-    [RequireComponent(typeof(Grid))]
-    public class Board : MonoBehaviour
+[RequireComponent(typeof(Grid))]
+public class Board : MonoBehaviour
+{
+    [SerializeField]
+    private Tile tilePrefab = default;
+    [SerializeField]
+    private Transform tilesContainer = default;
+
+
+    private Dictionary<Vector2Int, Tile> tiles = new Dictionary<Vector2Int, Tile>();
+
+    private ReadOnlyDictionary<Vector2Int, Tile> tilesDictionary = null;
+    public ReadOnlyDictionary<Vector2Int, Tile> TilesDictionary =>
+        tilesDictionary ?? (tilesDictionary = new ReadOnlyDictionary<Vector2Int, Tile>(tiles));
+
+    public int UsedTilesCount => tiles.Count;
+    public Tile this[int x, int y] => this[new Vector2Int(x, y)];
+    public Tile this[Vector2Int coordinates] => tiles[coordinates];
+
+    private BoundsInt? tileBounds = null;
+    public BoundsInt TileBounds => tileBounds ?? default;
+
+
+    private Grid grid;
+
+
+    public bool HasTile(Vector2Int coordinates) => tiles.ContainsKey(coordinates);
+
+
+    public Vector2Int GetTileCoordinatesFromWorldPoint(Vector2 position) => (Vector2Int)grid.WorldToCell(position);
+    public Tile GetTileFromWorldPoint(Vector2 position) => this[GetTileCoordinatesFromWorldPoint(position)];
+
+
+    private void Awake()
     {
-        [SerializeField]
-        private Tile tilePrefab = default;
-        [SerializeField]
-        private Transform tilesContainer = default;
-
-
-        private Dictionary<Vector2Int, Tile> tiles = new Dictionary<Vector2Int, Tile>();
-
-        private ReadOnlyDictionary<Vector2Int, Tile> tilesDictionary = null;
-        public ReadOnlyDictionary<Vector2Int, Tile> TilesDictionary =>
-            tilesDictionary ?? (tilesDictionary = new ReadOnlyDictionary<Vector2Int, Tile>(tiles));
-
-        public int UsedTilesCount => tiles.Count;
-        public Tile this [int x, int y] => this[new Vector2Int(x, y)];
-        public Tile this[Vector2Int coordinates] => tiles[coordinates];
-
-        private BoundsInt? tileBounds = null;
-        public BoundsInt TileBounds => tileBounds ?? default;
-
-
-        private Grid grid;
-
-
-        public bool HasTile(Vector2Int coordinates) => tiles.ContainsKey(coordinates);
-
-
-        public Vector2Int GetTileCoordinatesFromWorldPoint(Vector2 position) => (Vector2Int)grid.WorldToCell(position);
-        public Tile GetTileFromWorldPoint(Vector2 position) => this[GetTileCoordinatesFromWorldPoint(position)];
-
-
-        private void Awake()
-        {
-            grid = GetComponent<Grid>();
-        }
-
-
-        public void CreateBoard(IEnumerable<Vector2Int> coordinates)
-        {
-            foreach (Vector2Int coordinate in coordinates)
-                CreateTile(coordinate);
-        }
-
-        public Tile CreateTile(Vector2Int coordinates)
-        {
-            if (HasTile(coordinates))
-                return this[coordinates];
-
-            Tile newTile = Instantiate(tilePrefab, tilesContainer);
-            newTile.name = $"Tile ({coordinates.x}, {coordinates.y})";
-            newTile.transform.localPosition = grid.GetCellCenterLocal(new Vector3Int(coordinates.x, coordinates.y, 0));
-
-            newTile.Board = this;
-            newTile.Coordinates = coordinates;
-
-            EncapsulateToBounds(coordinates);
-
-            tiles.Add(coordinates, newTile);
-            return newTile;
-        }
-
-        /// <summary>
-        /// To extend the bounds to that point.
-        /// </summary>
-        /// <param name="point">The point to extend the bounds.</param>
-        private void EncapsulateToBounds(Vector2Int point)
-        {
-            if (tileBounds != null)
-            {
-                BoundsInt bounds = tileBounds.Value;
-                bounds.xMin = Mathf.Min(bounds.xMin, point.x);
-                bounds.xMax = Mathf.Max(bounds.xMax, point.x);
-                bounds.yMin = Mathf.Min(bounds.yMin, point.y);
-                bounds.yMax = Mathf.Max(bounds.yMax, point.y);
-                tileBounds = bounds;
-            }
-            else tileBounds = new BoundsInt((Vector3Int)point, Vector3Int.zero);
-        }
-
-
-        public Tile CreateTile(Vector2Int coordinates, Tile.Info info)
-        {
-            Tile newTile = CreateTile(coordinates);
-
-            newTile.TileType = info.type;
-            newTile.SpawneableForPlayerNumber = info.spawneableForPlayerNumber;
-
-            return newTile;
-        }
+        grid = GetComponent<Grid>();
     }
+
+
+    public void CreateBoard(IEnumerable<Vector2Int> coordinates)
+    {
+        foreach (Vector2Int coordinate in coordinates)
+            CreateTile(coordinate);
+    }
+
+    public Tile CreateTile(Vector2Int coordinates)
+    {
+        if (HasTile(coordinates))
+            return this[coordinates];
+
+        Tile newTile = Instantiate(tilePrefab, tilesContainer);
+        newTile.name = $"Tile ({coordinates.x}, {coordinates.y})";
+        newTile.transform.localPosition = grid.GetCellCenterLocal(new Vector3Int(coordinates.x, coordinates.y, 0));
+
+        newTile.Board = this;
+        newTile.Coordinates = coordinates;
+
+        EncapsulateToBounds(coordinates);
+
+        tiles.Add(coordinates, newTile);
+        //Debug.Log($"Created tile at {coordinates}.", newTile);
+        return newTile;
+    }
+
+    public Tile CreateTile(Vector2Int coordinates, Tile.Info info)
+    {
+        Tile newTile = CreateTile(coordinates);
+
+        newTile.TileType = info.type;
+        newTile.SpawneableForPlayerNumber = info.spawneableForPlayerNumber;
+
+        return newTile;
+    }
+
+    /// <summary>
+    /// To extend the bounds to that point.
+    /// </summary>
+    /// <param name="point">The point to extend the bounds.</param>
+    private void EncapsulateToBounds(Vector2Int point)
+    {
+        if (tileBounds != null)
+        {
+            BoundsInt bounds = tileBounds.Value;
+            bounds.xMin = Mathf.Min(bounds.xMin, point.x);
+            bounds.xMax = Mathf.Max(bounds.xMax, point.x);
+            bounds.yMin = Mathf.Min(bounds.yMin, point.y);
+            bounds.yMax = Mathf.Max(bounds.yMax, point.y);
+            tileBounds = bounds;
+        }
+        else tileBounds = new BoundsInt((Vector3Int)point, Vector3Int.zero);
+    }
+}
--- PanchosBattle/Assets/Scripts/TilemapToBoardGenerator.cs
+++ PanchosBattle/Assets/Scripts/TilemapToBoardGenerator.cs
@@ -2,62 +2,65 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-    [RequireComponent(typeof(Tilemap))]
-    public class TilemapToBoardGenerator : MonoBehaviour
+[RequireComponent(typeof(Tilemap))]
+public class TilemapToBoardGenerator : MonoBehaviour
+{
+    [Serializable]
+    private struct TileToTileType
     {
-        [Serializable]
-        private struct TileToTileType
+        public TileBase tile;
+        public Tile.Info info;
+
+        public TileToTileType(TileBase tile, Tile.Info info)
         {
-            public TileBase tile;
-            public Tile.Info info;
-
-            public TileToTileType(TileBase tile, Tile.Info info)
-            {
-                this.tile = tile ?? throw new ArgumentNullException(nameof(tile));
-                this.info = info;
-            }
-        }
-
-        private static readonly TileBase[] tiles = new TileBase[512];
-
-        [SerializeField]
-        private Board fillBoard = default;
-
-        [SerializeField]
-        private bool destroyOnGeneration = true;
-
-
-
-        private void Start()
-        {
-            GenerateFromTilemap();
-        }
-
-        private void GenerateFromTilemap()
-        {
-            // Fill the tiles with the corresponding tile types
-            Tilemap tilemap = GetComponent<Tilemap>();
-
-            BoundsInt bounds = tilemap.cellBounds;
-            for (int y = bounds.yMin; y < bounds.yMax; y++)
-            {
-                for (int x = bounds.xMin; x < bounds.xMax; x++)
-                {
-                    Vector3Int position = new Vector3Int(x, y, 0);
-                    if (tilemap.HasTile(position))
-                    {
-                        // Try to get the corresponding tile info depending on the tilemap tile
-                        if (tilemap.GetTile(position) is TilemapTileInfo tileWithInfo)
-                            fillBoard.CreateTile(new Vector2Int(x, y), tileWithInfo.TileInfo);
-                        else
-                            fillBoard.CreateTile(new Vector2Int(x, y));
-                    }
-                }
-            }
-
-            if (destroyOnGeneration)
-                Destroy(gameObject);
-            else
-                gameObject.SetActive(false);
+            this.tile = tile ?? throw new ArgumentNullException(nameof(tile));
+            this.info = info;
         }
     }
+
+    [SerializeField]
+    private Board fillBoard = default;
+
+    [SerializeField]
+    private bool destroyOnGeneration = true;
+
+
+
+    private void Start()
+    {
+        GenerateFromTilemap();
+    }
+
+    private void GenerateFromTilemap()
+    {
+        // Fill the tiles with the corresponding tile types
+        Tilemap tilemap = GetComponent<Tilemap>();
+
+        Debug.Log("Creating map from tilemap...");
+        var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+        BoundsInt bounds = tilemap.cellBounds;
+        for (int y = bounds.yMin; y < bounds.yMax; y++)
+        {
+            for (int x = bounds.xMin; x < bounds.xMax; x++)
+            {
+                Vector3Int position = new Vector3Int(x, y, 0);
+                if (tilemap.HasTile(position))
+                {
+                    // Try to get the corresponding tile info depending on the tilemap tile
+                    if (tilemap.GetTile(position) is TilemapTileInfo tileWithInfo)
+                        fillBoard.CreateTile(new Vector2Int(x, y), tileWithInfo.TileInfo);
+                    else
+                        fillBoard.CreateTile(new Vector2Int(x, y));
+                }
+            }
+        }
+        stopwatch.Stop();
+        Debug.Log($"Created map with bounds ({fillBoard.TileBounds}) in {stopwatch.Elapsed.TotalMilliseconds} ms" +
+            $" from a tilemap with bounds ({bounds}).");
+
+        if (destroyOnGeneration)
+            Destroy(gameObject);
+        else
+            gameObject.SetActive(false);
+    }
+}
