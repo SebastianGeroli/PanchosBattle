a3f407c278d8677252f24cefee7c93ae296c8053
Sebastian Geroli <sebastiangeroli@outlook.com>
2019-05-31 14:56:55 -0300

    funcionando con pantalla inicial

---
+++ PanchosBattle/Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+		onClick();
+    }
+	public void onClick() {
+		if( Input.GetMouseButtonDown( 0 ) ) {
+			SceneManager.LoadScene("Game");
+		}
+	}
+}
